ba356b7fde79544a648c2da762c4425cd212c4a2
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2024-03-08 01:29:07 -0500

    deploy-server change

--- deploy-server/index.js
+++ deploy-server/index.js
@@ -5,13 +5,15 @@ const Redis = require('ioredis');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const mime = require('mime-types');
 
-// const redis = new Redis({
-// 	host: 'localhost',
-// 	port: 6379,
-// });
+const redis = new Redis({
+	host: '',
+	port: 44739,
+	password: '',
+	tls: {}
+});
 
 const s3Client = new S3Client({
-	region: 'us-east-2',
+	region: '',
 	credentials: {
 		accessKeyId: '',
 		secretAccessKey: '',
@@ -19,14 +21,15 @@ const s3Client = new S3Client({
 });
 
 const PROJECT_ID = process.env.PROJECT_ID;
+const distFolder = process.env.DIST_FOLDER;
 
-// function publishLog(log : string) {
-// 	redis.publish(`logs from ${PROJECT_ID}`, JSON.stringify(log));
-// }
+function publishLog(log) {
+	redis.publish(`logs:${PROJECT_ID}`, JSON.stringify({log}));
+}
 
 async function init() {
 	console.log('Executing scripts');
-	// publishLog('Executing scripts');
+	publishLog('Executing scripts');
 
 	console.log('dirname', __dirname);
 	const outDirPath = path.join(__dirname, 'output');
@@ -36,18 +39,18 @@ async function init() {
 
 	p.stdout?.on('data', (data) => {
 		console.log(data.toString());
-		// publishLog(data.toString());
+		publishLog(data.toString());
 	});
 
 	p.stdout?.on('error', (data) => {
 		console.log(data.toString());
-		// publishLog(`error: ${data.toString()}`);
+		publishLog(`error: ${data.toString()}`);
 	});
 
 	p.on('close', async () => {
 		console.log('Build done');
-		// publishLog('Build done');
-		const distFolderPath = path.join(__dirname, 'output', 'dist');
+		publishLog('Build done');
+		const distFolderPath = path.join(__dirname, 'output', `${distFolder}`);
 
 		if (!fs.existsSync(distFolderPath)) {
 			console.log(
@@ -62,14 +65,14 @@ async function init() {
 		});
 		console.log('distFolderContents:', distFolderContents.toString());
 
-		// publishLog(`Starting to upload`);
+		publishLog(`Starting to upload`);
 
 		for (const file of distFolderContents) {
 			const filePath = path.join(distFolderPath, file);
 			if (fs.lstatSync(filePath).isDirectory()) continue;
 
 			console.log('uploading', filePath);
-			// publishLog(`uploading ${file}`);
+			publishLog(`uploading ${file}`);
 
 			const params = new PutObjectCommand({
 				Bucket: 'nova-user-files-bucket',
@@ -80,11 +83,13 @@ async function init() {
 
 			await s3Client.send(params);
 
-			// publishLog(`uploaded ${file}`);
+			publishLog(`uploaded ${file}`);
 			console.log('uploaded', filePath);
 		}
-		// publishLog(`Done`);
+		publishLog(`Done`);
 		console.log('Done...');
+		// Close the redis connection so the task will auto finish
+		await redis.quit();
 	});
 }
 
